3fb79270f644d187474e8dd3de2fa781e6fdc15c
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-14 19:42:14 +0500

    crud

---
+++ MockProject/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MockProject.Dtos;
+using MockProject.Services;
+
+namespace MockProject.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateUserAsync( UserDto userDto)
+        {
+            var res = await _userService.CreateUserAsync(userDto);
+            return Ok(res);
+        }
+
+        [HttpGet]
+
+        public async ValueTask<IActionResult> GetAllAsync()
+        {
+            var res = await _userService.GetAllAsync();
+            return Ok(res);
+        }
+
+        [HttpGet]
+
+        public async ValueTask<IActionResult> GetByIdAsync(int Id) 
+        {
+            var res = await _userService.GetByIdAsync(Id);
+            return Ok(res);
+        }
+
+        [HttpPut]
+
+        public async ValueTask<IActionResult> UpdateUserAsync(int Id, UserDto userDto)
+        {
+            var res = await _userService.UpdateUserAsync(Id,userDto);
+            return Ok(res);
+        }
+
+        [HttpDelete]
+
+        public async ValueTask<IActionResult> DeleteUserAsync(int Id)
+        {
+            var res = await _userService.DeleteUserAsync(Id);
+            return Ok(res);
+        }
+    }
+}
