f263956a35e47a82a9ebd28017653ca716de3194
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-14 19:41:49 +0500

    crud

---
+++ MockProject/Repositories/UserRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MockProject.DataAccess;
+using MockProject.Entities;
+
+namespace MockProject.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbConntext _appDbConntext;
+
+        public UserRepository(AppDbConntext appDbConntext)
+        {
+            _appDbConntext = appDbConntext;
+        }
+
+        public async ValueTask<bool> CreateUserAsync(User user)
+        {
+            await _appDbConntext.Users.AddAsync(user);
+            var result = await _appDbConntext.SaveChangesAsync();
+            return result > 0;
+        }
+
+        public async ValueTask<bool> DeleteUserAsync(int id)
+        {
+            var res = await _appDbConntext.Users.FirstOrDefaultAsync(x => x.Id == id);
+            _appDbConntext.Users.Remove(res);
+            var result = await _appDbConntext.SaveChangesAsync();
+            return result > 0;
+        }
+
+        public async ValueTask<List<User>> GetAllAsync()
+        {
+            var res = await _appDbConntext.Users.ToListAsync();
+            return res;
+        }
+
+        public async ValueTask<User> GetByIdAsync(int id)
+        {
+            var res =  await _appDbConntext.Users.FirstOrDefaultAsync(x => x.Id == id);
+            return res ;
+        }
+
+        public async ValueTask<bool> UpdateUserAsync(User user)
+        {
+            var res = await _appDbConntext.Users.FirstOrDefaultAsync(x =>x.Id == user.Id);
+            if (res == null) { return false; }
+
+            _appDbConntext.Users.Update(res);
+            var result = await _appDbConntext.SaveChangesAsync();
+            return result > 0;
+        }
+    }
+}
