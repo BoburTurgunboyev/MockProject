eaf1c165ce81bbb850252b8e22c4c93e18e930ee
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-14 18:10:50 +0500

    datbasa qoshildi

---
+++ MockProject/DataAccess/AppDbConntext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MockProject.Entities;
+
+namespace MockProject.DataAccess
+{
+    public class AppDbConntext : DbContext
+    {
+        public AppDbConntext(DbContextOptions<AppDbConntext> options) :base(options)
+        {
+            
+        }
+
+
+        public DbSet<User> Users { get; set; } 
+    }
+}
