6949a247a25e991738c52571c1b5fe3129af160f
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-12-14 19:41:42 +0500

    crud

---
+++ MockProject/Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using MockProject.Dtos;
+using MockProject.Entities;
+
+namespace MockProject.Services
+{
+    public interface IUserService
+    {
+        public ValueTask<bool> CreateUserAsync(UserDto userDto);
+
+        public ValueTask<bool> UpdateUserAsync(int Id,UserDto userDto);
+        public ValueTask<bool> DeleteUserAsync(int Id);
+        public ValueTask<List<User>> GetAllAsync();
+        public ValueTask<User> GetByIdAsync(int Id);
+    }
+}
